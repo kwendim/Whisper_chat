e9832e28ace2792d9c4d86f74df2ef72c5737b31
Mohammad Elhariry <mohammad.elhariry@aalto.fi>
2018-11-16 14:17:53 +0200

    Add missing files

---
+++ frontend/app/src/main/java/mcc_2018_g15/chatapp/MainActivity.java
@@ -0,0 +1,13 @@
+package mcc_2018_g15.chatapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
